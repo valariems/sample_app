522cc23c751406e8c3c20dfa0d45d9f26ed883b0
valariems <valarie@martinstuart.com>
2013-12-02 09:38:01 -0600

    Finish static pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "Static pages" do
       visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Trails Tutorial Sample App | Home')
+    end
   end
 
   describe "Help page" do
@@ -14,6 +19,11 @@ describe "Static pages" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Trails Tutorial Sample App | Help')
+    end
   end
 
   describe "About page" do
@@ -21,5 +31,10 @@ describe "Static pages" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About Us')
   	end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Trails Tutorial Sample App | About Us')
+    end
   end
 end
