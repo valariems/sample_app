8b9fa1b1422a111aab9d743682331dc0b0e54a2c
valariems <valarie@martinstuart.com>
2014-01-03 23:21:56 -0600

    Finish sign in.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,10 +27,10 @@ describe "AuthenticationPages" do
 				click_button "Sign in"
 			end
 
-			it { should have_title("user.name") }
+			it { should have_title(user.name) }
 			it { should have_link("Profile", href: user_path(user)) }
-			it { should have_link("Sign out", href: signout_path) }
-			it { should_not have_link("Sign in", href: signin_path) }
+			it { should have_link("Sign Out", href: signout_path) }
+			it { should_not have_link("Sign In", href: signin_path) }
 		end
 	end
 end
